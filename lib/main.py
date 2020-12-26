b3f9c4e8d44dfed4f9c2311a2d64a34832083fac
Brando <brandoflores16@gmail.com>
2020-12-25 23:36:42 -0700

    Add text field input and write to data file.

--- lib/main.py
+++ lib/main.py
@@ -3,6 +3,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,13 +39,14 @@ website_entry = Entry(width=35)
 website_entry.grid(row=1, column = 1, columnspan=2)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "test@email.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
